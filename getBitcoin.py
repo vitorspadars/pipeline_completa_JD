052d5d7389c755d8a1f270304b52a48df80cda7a
vitorspada <vitorspada.rs@gmail.com>
2025-10-26 19:36:20 -0300

    codigo para pegar dados da api de bitcoin

---
+++ getBitcoin.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+import pandas as pd
+
+
+# URL
+url = "https://api.coinbase.com/v2/prices/spot"
+
+# GET para pegar os dados da API
+response = requests.get(url)
+data = response.json()
+
+#Extrair os dados que eu quero
+preco = float(data['data']['amount'])
+ativo = data['data']['base']
+moeda = data['data']['currency']
+horario = datetime.now()
+
+df = pd.DataFrame([{
+    'ativo' : ativo,
+    'preco' : preco,
+    'moeda' : moeda,
+    'horario' : horario
+}])
+
